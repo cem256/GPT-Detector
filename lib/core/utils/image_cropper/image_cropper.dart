980e7dc83e038ce63cd3e6eaca40adf59cfcac37
cem256 <cemo99@gmail.com>
2023-02-13 23:27:40 +0300

    release: version 1.0.2

--- lib/core/utils/image_cropper/image_cropper.dart
+++ lib/core/utils/image_cropper/image_cropper.dart
@@ -17,28 +17,36 @@ class ImageCropperUtilsImpl implements ImageCropperUtils {
     final file = await _imageCropper.cropImage(
       sourcePath: filePath,
       compressQuality: 100,
-      aspectRatioPresets: [
-        CropAspectRatioPreset.original,
-        CropAspectRatioPreset.square,
-        CropAspectRatioPreset.ratio4x3,
-        CropAspectRatioPreset.ratio16x9,
-      ],
-      uiSettings: [
-        AndroidUiSettings(
-          toolbarTitle: 'Crop Photo',
-          toolbarColor: getIt<AppTheme>().theme.primaryColor,
-          toolbarWidgetColor: getIt<AppTheme>().theme.scaffoldBackgroundColor,
-          activeControlsWidgetColor: getIt<AppTheme>().theme.primaryColor,
-          initAspectRatio: CropAspectRatioPreset.original,
-          lockAspectRatio: false,
-          showCropGrid: false,
-        ),
-        IOSUiSettings(
-          title: 'Crop Photo',
-        )
-      ],
+      aspectRatioPresets: _aspectRatioPresets,
+      uiSettings: _uiSettings,
     );
 
     return file?.path;
+  }
+
+  List<CropAspectRatioPreset> get _aspectRatioPresets {
+    return [
+      CropAspectRatioPreset.original,
+      CropAspectRatioPreset.square,
+      CropAspectRatioPreset.ratio4x3,
+      CropAspectRatioPreset.ratio16x9,
+    ];
+  }
+
+  List<PlatformUiSettings> get _uiSettings {
+    return [
+      AndroidUiSettings(
+        toolbarTitle: 'Edit Photo',
+        toolbarColor: getIt<AppTheme>().theme.primaryColor,
+        toolbarWidgetColor: getIt<AppTheme>().theme.scaffoldBackgroundColor,
+        activeControlsWidgetColor: getIt<AppTheme>().theme.primaryColor,
+        initAspectRatio: CropAspectRatioPreset.original,
+        lockAspectRatio: false,
+        showCropGrid: false,
+      ),
+      IOSUiSettings(
+        title: 'Edit Photo',
+      )
+    ];
   }
 }
