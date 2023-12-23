6f120661a02f46574dbdfabe1c613952d0b44dca
cem256 <cemo99@gmail.com>
2023-12-23 14:38:00 +0300

    add base cubit

--- lib/feature/onboarding/presentation/cubit/onboarding_cubit.dart
+++ lib/feature/onboarding/presentation/cubit/onboarding_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:gpt_detector/app/base/cubit/base_cubit.dart';
 import 'package:gpt_detector/feature/onboarding/domain/use_cases/complete_onboarding_use_case.dart';
 
 import 'package:injectable/injectable.dart';
@@ -8,7 +8,7 @@ part 'onboarding_state.dart';
 part 'onboarding_cubit.freezed.dart';
 
 @injectable
-class OnboardingCubit extends Cubit<OnboardingState> {
+class OnboardingCubit extends BaseCubit<OnboardingState> {
   OnboardingCubit({required CompleteOnboardingUseCase completeOnboardingUseCase})
       : _completeOnboardingUseCase = completeOnboardingUseCase,
         super(const OnboardingState());
