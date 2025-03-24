7302fe6994572ae748a48edf1a8e4121273d8b04
cem256 <cemo99@gmail.com>
2025-03-24 11:26:14 +0300

    1.3.4+36, rename "GPT Detector" to "AI Detector"

--- lib/app/constants/string_constants.dart
+++ lib/app/constants/string_constants.dart
@@ -1,5 +1,5 @@
 abstract final class StringConstants {
-  static const String appName = 'GPT Detector';
+  static const String appName = 'AI Detector';
   static const String passwordGenerator = 'Password Generator';
   static const String privacyPolicyUrl = 'https://cem256.github.io/GPT-Detector/privacy-policy.html';
   static const String githubUrl = 'https://github.com/cem256/GPT-Detector';
